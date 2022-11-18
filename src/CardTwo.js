9c5f7c4b7e272ae6757cbb5b9bb207203e9a74a3
Henry <adolphosita@yahoo.com>
2022-11-18 11:40:15 +0100

    feat:App.js, CardOne.js, CardTwo.js

---
+++ src/CardTwo.js
@@ -0,0 +1,316 @@
+import Stars from "./images/stars.svg";
+
+const CardOne = () => {
+  return (
+    <>
+      <section className="flex gap-4 justify-center items-center flex-wrap px-3 lg:px-20 lg:p-10 mb-16">
+        <div className="border border-[#D7D7D7] rounded-lg p-3 text-xs">
+          <img
+            src="card1/card1b.svg"
+            alt="profile"
+            className="object-contain w-[300px] h-auto"
+          />
+          <div className="flex justify-between mt-3">
+            <p>Desert King</p>
+            <p className="font-semibold">1MBT per night</p>
+          </div>
+          <div className="flex justify-between mt-3 ">
+            <p>2345Km away</p>
+            <p>available for 2weeks stay</p>
+          </div>
+          <div className="flex mt-2 gap-1.5">
+            <img src={Stars} alt="Profile" />
+          </div>
+        </div>
+
+        <div className="border border-[#D7D7D7] rounded-lg p-3 text-xs">
+          <img
+            src="/card1/card1b.svg"
+            alt="Profile"
+            className="object-contain w-[300px] h-auto"
+          />
+          <div className="flex justify-between mt-3">
+            <p>Desert King</p>
+            <p className="font-semibold">1MBT per night</p>
+          </div>
+          <div className="flex justify-between mt-3 ">
+            <p>2345Km away</p>
+            <p>available for 2weeks stay</p>
+          </div>
+          <div className="flex mt-2 gap-1.5">
+            <img src={Stars} alt="Profile" />
+          </div>
+        </div>
+
+        <div className="border border-[#D7D7D7] rounded-lg p-3 text-xs">
+          <img
+            src="/card1/card1c.svg"
+            alt=""
+            className="object-contain w-[300px] h-auto"
+          />
+          <div className="flex justify-between mt-3">
+            <p>Desert King</p>
+            <p className="font-semibold">1MBT per night</p>
+          </div>
+          <div className="flex justify-between mt-3 ">
+            <p>2345Km away</p>
+            <p>available for 2weeks stay</p>
+          </div>
+          <div className="flex mt-2 gap-1.5">
+            <img src={Stars} alt="Profile" />
+          </div>
+        </div>
+
+        <div className="border border-[#D7D7D7] rounded-lg p-3 text-xs">
+          <img
+            src="/card1/card1d.svg"
+            alt=""
+            className="object-contain w-[300px] h-auto"
+          />
+          <div className="flex justify-between mt-3">
+            <p>Desert King</p>
+            <p className="font-semibold">1MBT per night</p>
+          </div>
+          <div className="flex justify-between mt-3 ">
+            <p>2345Km away</p>
+            <p>available for 2weeks stay</p>
+          </div>
+          <div className="flex mt-2 gap-1.5">
+            <img src={Stars} alt="Profile" />
+          </div>
+        </div>
+
+        <div className="border border-[#D7D7D7] rounded-lg p-3 text-xs">
+          <img
+            src="/card1/card1e.svg"
+            alt=""
+            className="object-contain w-[300px] h-auto"
+          />
+          <div className="flex justify-between mt-3">
+            <p>Desert King</p>
+            <p className="font-semibold">1MBT per night</p>
+          </div>
+          <div className="flex justify-between mt-3 ">
+            <p>2345Km away</p>
+            <p>available for 2weeks stay</p>
+          </div>
+          <div className="flex mt-2 gap-1.5">
+            <img src={Stars} alt="Profile" />
+          </div>
+        </div>
+
+        <div className="border border-[#D7D7D7] rounded-lg p-3 text-xs">
+          <img
+            src="/card1/card1f.svg"
+            alt=""
+            className="object-contain w-[300px] h-auto"
+          />
+          <div className="flex justify-between mt-3">
+            <p>Desert King</p>
+            <p className="font-semibold">1MBT per night</p>
+          </div>
+          <div className="flex justify-between mt-3 ">
+            <p>2345Km away</p>
+            <p>available for 2weeks stay</p>
+          </div>
+          <div className="flex mt-2 gap-1.5">
+            <img src={Stars} alt="Profile" />
+          </div>
+        </div>
+
+        <div className="border border-[#D7D7D7] rounded-lg p-3 text-xs">
+          <img
+            src="/card1/card1g.svg"
+            alt=""
+            className="object-contain w-[300px] h-auto"
+          />
+          <div className="flex justify-between mt-3">
+            <p>Desert King</p>
+            <p className="font-semibold">1MBT per night</p>
+          </div>
+          <div className="flex justify-between mt-3 ">
+            <p>2345Km away</p>
+            <p>available for 2weeks stay</p>
+          </div>
+          <div className="flex mt-2 gap-1.5">
+            <img src={Stars} alt="Profile" />
+          </div>
+        </div>
+
+        <div className="border border-[#D7D7D7] rounded-lg p-3 text-xs">
+          <img
+            src="/card1/card1h.svg"
+            alt=""
+            className="object-contain w-[300px] h-auto"
+          />
+          <div className="flex justify-between mt-3">
+            <p>Desert King</p>
+            <p className="font-semibold">1MBT per night</p>
+          </div>
+          <div className="flex justify-between mt-3 ">
+            <p>2345Km away</p>
+            <p>available for 2weeks stay</p>
+          </div>
+          <div className="flex mt-2 gap-1.5">
+            <img src={Stars} alt="Profile" />
+          </div>
+        </div>
+
+        <div className="border border-[#D7D7D7] rounded-lg p-3 text-xs">
+          <img
+            src="/card1/card1g.svg"
+            alt=""
+            className="object-contain w-[300px] h-auto"
+          />
+          <div className="flex justify-between mt-3">
+            <p>Desert King</p>
+            <p className="font-semibold">1MBT per night</p>
+          </div>
+          <div className="flex justify-between mt-3 ">
+            <p>2345Km away</p>
+            <p>available for 2weeks stay</p>
+          </div>
+          <div className="flex mt-2 gap-1.5">
+            <img src={Stars} alt="Profile" />
+          </div>
+        </div>
+
+        <div className="border border-[#D7D7D7] rounded-lg p-3 text-xs">
+          <img
+            src="/card1/card1g.svg"
+            alt=""
+            className="object-contain w-[300px] h-auto"
+          />
+          <div className="flex justify-between mt-3">
+            <p>Desert King</p>
+            <p className="font-semibold">1MBT per night</p>
+          </div>
+          <div className="flex justify-between mt-3 ">
+            <p>2345Km away</p>
+            <p>available for 2weeks stay</p>
+          </div>
+          <div className="flex mt-2 gap-1.5">
+            <img src={Stars} alt="Profile" />
+          </div>
+        </div>
+        <div className="border border-[#D7D7D7] rounded-lg p-3 text-xs">
+          <img
+            src="/card1/card1g.svg"
+            alt=""
+            className="object-contain w-[300px] h-auto"
+          />
+          <div className="flex justify-between mt-3">
+            <p>Desert King</p>
+            <p className="font-semibold">1MBT per night</p>
+          </div>
+          <div className="flex justify-between mt-3 ">
+            <p>2345Km away</p>
+            <p>available for 2weeks stay</p>
+          </div>
+          <div className="flex mt-2 gap-1.5">
+            <img src={Stars} alt="Profile" />
+          </div>
+        </div>
+
+        <div className="border border-[#D7D7D7] rounded-lg p-3 text-xs">
+          <img
+            src="/card1/card1g.svg"
+            alt=""
+            className="object-contain w-[300px] h-auto"
+          />
+          <div className="flex justify-between mt-3">
+            <p>Desert King</p>
+            <p className="font-semibold">1MBT per night</p>
+          </div>
+          <div className="flex justify-between mt-3 ">
+            <p>2345Km away</p>
+            <p>available for 2weeks stay</p>
+          </div>
+          <div className="flex mt-2 gap-1.5">
+            <img src={Stars} alt="Profile" />
+          </div>
+        </div>
+
+        <div className="border border-[#D7D7D7] rounded-lg p-3 text-xs">
+          <img
+            src="/card1/card1g.svg"
+            alt=""
+            className="object-contain w-[300px] h-auto"
+          />
+          <div className="flex justify-between mt-3">
+            <p>Desert King</p>
+            <p className="font-semibold">1MBT per night</p>
+          </div>
+          <div className="flex justify-between mt-3 ">
+            <p>2345Km away</p>
+            <p>available for 2weeks stay</p>
+          </div>
+          <div className="flex mt-2 gap-1.5">
+            <img src={Stars} alt="Profile" />
+          </div>
+        </div>
+
+        <div className="border border-[#D7D7D7] rounded-lg p-3 text-xs">
+          <img
+            src="/card1/card1g.svg"
+            alt=""
+            className="object-contain w-[300px] h-auto"
+          />
+          <div className="flex justify-between mt-3">
+            <p>Desert King</p>
+            <p className="font-semibold">1MBT per night</p>
+          </div>
+          <div className="flex justify-between mt-3 ">
+            <p>2345Km away</p>
+            <p>available for 2weeks stay</p>
+          </div>
+          <div className="flex mt-2 gap-1.5">
+            <img src={Stars} alt="Profile" />
+          </div>
+        </div>
+
+        <div className="border border-[#D7D7D7] rounded-lg p-3 text-xs">
+          <img
+            src="/card1/card1g.svg"
+            alt=""
+            className="object-contain w-[300px] h-auto"
+          />
+          <div className="flex justify-between mt-3">
+            <p>Desert King</p>
+            <p className="font-semibold">1MBT per night</p>
+          </div>
+          <div className="flex justify-between mt-3 ">
+            <p>2345Km away</p>
+            <p>available for 2weeks stay</p>
+          </div>
+          <div className="flex mt-2 gap-1.5">
+            <img src={Stars} alt="Profile" />
+          </div>
+        </div>
+
+        <div className="border border-[#D7D7D7] rounded-lg p-3 text-xs">
+          <img
+            src="/card1/card1g.svg"
+            alt=""
+            className="object-contain w-[300px] h-auto"
+          />
+          <div className="flex justify-between mt-3">
+            <p>Desert King</p>
+            <p className="font-semibold">1MBT per night</p>
+          </div>
+          <div className="flex justify-between mt-3 ">
+            <p>2345Km away</p>
+            <p>available for 2weeks stay</p>
+          </div>
+          <div className="flex mt-2 gap-1.5">
+            <img src={Stars} alt="Profile" />
+          </div>
+        </div>
+
+        
+      </section>
+    </>
+  );
+};
+
+export default CardOne;
